9e677630ba9b709d6071b56666cbf89bd3eddd91
Mr.s <shuo4413@icloud.com>
2020-08-14 13:56:01 +0800

    显示个人资料

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable implements MustVerifyEmailController
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','introduction',
     ];
 
     /**
