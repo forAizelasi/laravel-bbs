931e26c04072a24da6d8ce6b7d1ec8cfda2f285d
Mr.s <shuo4413@icloud.com>
2020-08-19 10:31:00 +0800

    站点权限部署

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,10 @@
 
   </div>
 
+  @if(app ()->isLocal())
+    @include('sudosu::user-selector')
+    @endif
+
 <!-- Scripts -->
   <script src="{{ mix ('js/app.js') }}"></script>
 @yield('scripts')
