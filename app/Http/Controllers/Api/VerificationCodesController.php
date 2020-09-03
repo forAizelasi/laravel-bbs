c8c68ec0ed81634cb6aca84c697e728dbb56319a
Mr.s <shuo4413@icloud.com>
2020-09-03 10:29:18 +0800

    图片验证码

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\VerificationCodeRequest;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Overtrue\EasySms\EasySms;
@@ -12,7 +13,21 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+
+        $captchaData = \Cache::get ($request->captcha_key);
+
+        if (!$captchaData) {
+            abort (403,'图片验证码失效');
+        }
+
+        if (!hash_equals ($captchaData['code'],$request->captcha_code)) {
+            var_dump($captchaData['code']);
+            var_dump($request->captcha_code);
+            //验证错误就清除缓存
+            \Cache::forget ($request->captcha_key);
+            throw new AuthenticationException('验证码错误');
+        }
+        $phone = $captchaData['phone'];
 
         if (!app ()->environment ('production')) {
             $code = '1234';
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix ('v1')->namespace ('Api')->name ('api.v1.')->group (function () {
     Route::middleware ('throttle:'.config ('api.rate_limits.sign'))->group (function () {
+        //图片验证码
+        Route::post ('captchas','CaptchasController@store')->name ('captchas.store');
         //短信验证码
         Route::post ('verificationCodes','VerificationCodesController@store')->name ('verificationCodes.store');
         //用户注册
